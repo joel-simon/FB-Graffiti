f6100dbd1404a9241f279ebdd2d860f9c3e9b776
Joel Simon <joelsimon6@gmail.com>
2014-04-30 20:53:17 -0400

    first

---
+++ server.js
@@ -0,0 +1,80 @@
+var AWS = require('aws-sdk'); 
+var fs = require('fs');
+var sys = require('sys');
+AWS.config.loadFromPath('./config.json');
+var s3 = new AWS.S3();
+var images = require ('images');
+AWS.config.loadFromPath('./config.json');
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+	var express = require('express');
+	var app = express();
+	var bodyParser = require('body-parser');
+	app.use(bodyParser({limit: '50mb'})); //
+
+	function getImage(path, cb) {
+		var params = {
+			Bucket: 'facebookGraffiti',
+			Key: 		path
+		};
+		s3.getObject(params, cb);
+	}
+
+	app.post('/setImage', function(req, res) {
+		
+		var path = req.body.path;
+		var img = req.body.img;
+		var data = img.replace(/^data:image\/\w+;base64,/, "");
+		var delta = images(new Buffer(data, 'base64'));
+		var start = new Date().getTime();
+		res.send('hello world');
+		getImage(path, function(err, data) {
+
+			var newImage;
+			// this post has not been drawn on before.
+			if (err) {
+				console.log('New image');
+				newImage = (delta).encode("png");
+
+			} else { // append the changes
+				console.log('Appending to old image');
+				var oldImg = images(data.Body);
+				if (delta.height() > oldImg.height()) { // need to englarge image.
+					console.log('need to expand image');
+					newImage = images(delta.width(), delta.height()).draw(oldImg,0,0).draw(delta,0,0).encode("png");
+				} else {
+					newImage = oldImg.draw(delta,0,0).encode("png");
+				}
+			}
+			var params = {
+				Bucket: 'facebookGraffiti',
+				Key: 		path,
+				Body: 	newImage,
+				ACL: 		'public-read'
+			};
+			s3.putObject(params, function(err, data) {
+			  if (err) {
+				  console.log(err);
+			  } else {
+			  	var end = new Date().getTime();
+					var time = end - start;
+			  	console.log("Successfully uploaded \n\t URL:", path, "\n\tIn: ", time);   
+			  }
+		  });
+		});
+	});
+
+	app.listen(3000);
+
+}
